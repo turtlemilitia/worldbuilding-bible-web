d66b6caeee490a9db74528bc5e5ebd2d5440dab2
flipersclub <lffonseca1@me.com>
2023-07-13 12:50:30 +0100

    Splitting React app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, {JSX, useState} from 'react';
-import logo from './logo.svg';
+import hamburger from './assets/hamburger.svg';
 import './App.css';
+import LoginBox from "./components/LoginBox/LoginBox";
 
-function App(): JSX.Element {
+const App = (): JSX.Element => {
   const [open, setOpen] = useState(false);
   return (
     <div className="App">
@@ -30,10 +31,7 @@ function App(): JSX.Element {
             <div className="absolute top-0 w-60 bg-white p-6">
               <div className="flex space-x-6 mb-6">
         <span onClick={() => setOpen(!open)}>
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24"
-               stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
-          </svg>
+          {hamburger}
         </span>
                 <h1>Dashboard</h1>
               </div>
@@ -50,34 +48,7 @@ function App(): JSX.Element {
       </header>
       <div className="flex h-screen w-full items-center justify-center bg-gray-900 bg-cover bg-no-repeat bg-center"
            style={{backgroundImage: "url('https://images.unsplash.com/photo-1499123785106-343e69e68db1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1748&q=80')"}}>
-        <div className="rounded-xl bg-gray-800 bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
-          <div className="text-white">
-            <div className="mb-8 flex flex-col items-center">
-              <img src="https://www.logo.wine/a/logo/Instagram/Instagram-Glyph-Color-Logo.wine.svg" width="150" alt=""
-                   srcSet=""/>
-              <h1 className="mb-2 text-2xl">Instagram</h1>
-              <span className="text-gray-300">Enter Login Details</span>
-            </div>
-            <form action="#">
-              <div className="mb-4 text-lg">
-                <input
-                  className="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md"
-                  type="text" name="name" placeholder="id@email.com"/>
-              </div>
-
-              <div className="mb-4 text-lg">
-                <input
-                  className="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md"
-                  type="Password" name="name" placeholder="*********"/>
-              </div>
-              <div className="mt-8 flex justify-center text-lg text-black">
-                <button type="submit"
-                        className="rounded-3xl bg-yellow-400 bg-opacity-50 px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-yellow-600">Login
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
+        <LoginBox/>
       </div>
     </div>
   );
