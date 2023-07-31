a42229fc1ef53f2872a952c077a0396f515b0945
filipefonseca <lffonseca1@me.com>
2023-07-31 08:08:23 +0100

    Simple Markdown Editor

--- src/components/Forms/SpyFields/DiscreetTextareaField.tsx
+++ src/components/Forms/SpyFields/DiscreetTextareaField.tsx
@@ -1,17 +1,21 @@
-import React, {DetailedHTMLProps, JSX, TextareaHTMLAttributes} from "react";
+import React, {JSX} from "react";
+import SimpleMdeReact from 'react-simplemde-editor'
 
-
-interface EmailFieldProps extends Omit<DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>, 'onChange'> {
-  onChange: (value: string) => void;
+interface EmailFieldProps {
+  value: string;
+  onChange: (value: string) => any;
+  placeholder?: string;
 }
 
 const DiscreetTextareaField = (props: EmailFieldProps): JSX.Element => {
-  const {onChange} = props
+  const {placeholder} = props
   return (
-    <textarea
+    <SimpleMdeReact
       {...props}
-      className="w-full border-none bg-transparent px-3 py-3 text-inherit placeholder:text-stone-400 outline-none"
-      onChange={(e) => onChange(e.target.value)}
+      textareaProps={{
+        placeholder,
+        className: "w-full border-none bg-transparent px-3 py-3 text-inherit placeholder:text-stone-400 outline-none"
+      }}
     />
   )
 }
