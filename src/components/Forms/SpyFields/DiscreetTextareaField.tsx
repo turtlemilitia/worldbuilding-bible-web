c36c4b169d4d55d5e9e8c5327355ec326333b507
filipefonseca <lffonseca1@me.com>
2023-07-31 15:22:43 +0100

    SimpleMde editor sort of working now

--- src/components/Forms/SpyFields/DiscreetTextareaField.tsx
+++ src/components/Forms/SpyFields/DiscreetTextareaField.tsx
@@ -1,5 +1,11 @@
-import React, {JSX} from "react";
+import React, { JSX, useCallback, useEffect, useMemo, useState } from 'react'
 import SimpleMdeReact from 'react-simplemde-editor'
+import SimpleMDE from "easymde";
+import type { Editor, EditorEventMap, KeyMap, Position } from "codemirror";
+import "easymde/dist/easymde.min.css";
+import ReactMarkdown from 'react-markdown'
+import { renderToString } from 'react-dom/server'
+import remarkGfm from 'remark-gfm'
 
 interface EmailFieldProps {
   value: string;
@@ -8,12 +14,64 @@ interface EmailFieldProps {
 }
 
 const DiscreetTextareaField = (props: EmailFieldProps): JSX.Element => {
-  const {placeholder} = props
+
+  // simple mde
+  const [simpleMdeInstance, setMdeInstance] = useState<SimpleMDE | null>(null);
+
+  const getMdeInstanceCallback = useCallback((simpleMde: SimpleMDE) => {
+    setMdeInstance(simpleMde);
+  }, []);
+
+  useEffect(() => {
+    simpleMdeInstance &&
+    console.info("Hey I'm editor instance!", simpleMdeInstance);
+  }, [simpleMdeInstance]);
+
+  // codemirror
+  const [codemirrorInstance, setCodemirrorInstance] = useState<Editor | null>(
+    null
+  );
+  const getCmInstanceCallback = useCallback((editor: Editor) => {
+    setCodemirrorInstance(editor);
+  }, []);
+
+  useEffect(() => {
+    codemirrorInstance &&
+    console.info("Hey I'm codemirror instance!", codemirrorInstance);
+  }, [codemirrorInstance]);
+
+  // line and cursor
+  const [lineAndCursor, setLineAndCursor] = useState<Position | null>(null);
+
+  const getLineAndCursorCallback = useCallback((position: Position) => {
+    setLineAndCursor(position);
+  }, []);
+
+  useEffect(() => {
+    lineAndCursor &&
+    console.info("Hey I'm line and cursor info!", lineAndCursor);
+  }, [lineAndCursor]);
+
+  const customRendererOptions = useMemo(() => {
+    return {
+      previewRender() {
+        return renderToString(
+          <ReactMarkdown remarkPlugins={[[remarkGfm, {singleTilde: true}]]}>
+            {props.value}
+          </ReactMarkdown>
+        );
+      },
+    } as SimpleMDE.Options;
+  }, []);
+
   return (
     <SimpleMdeReact
       {...props}
+      options={customRendererOptions}
+      getMdeInstance={getMdeInstanceCallback}
+      getCodemirrorInstance={getCmInstanceCallback}
+      getLineAndCursor={getLineAndCursorCallback}
       textareaProps={{
-        placeholder,
         className: "w-full border-none bg-transparent px-3 py-3 text-inherit placeholder:text-stone-400 outline-none"
       }}
     />
