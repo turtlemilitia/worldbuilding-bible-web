13d5f164b18fcb0cb207821d303498bc8dd0635c
filipefonseca <lffonseca1@me.com>
2023-12-21 22:27:19 +0000

    Extra buttons in music player

--- src/components/MusicPlayer/component.tsx
+++ src/components/MusicPlayer/component.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent, useState } from 'react'
-import { ActivityIcon } from 'lucide-react'
+import { ActivityIcon, PlayIcon, SkipBackIcon, SkipForwardIcon } from 'lucide-react'
 
 const MusicPlayer: FunctionComponent = () => {
 
@@ -7,12 +7,19 @@ const MusicPlayer: FunctionComponent = () => {
 
   return (
     <div
-      className={`fixed bottom-4 right-4 rounded-md shadow-sm shadow-stone-800 bg-stone-400 bg-opacity-10 text-stone-200 px-4 py-2 backdrop-blur-sm cursor-pointer flex flex-row`}
+      className={`fixed bottom-4 right-4 rounded-full shadow-md shadow-stone-950 border border-opacity-30 border-stone-400 bg-stone-400 bg-opacity-10 text-stone-200 px-3 py-3 backdrop-blur-sm cursor-pointer flex flex-row`}
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
     >
-      <ActivityIcon size={15}/>
-      <div className={`${hovered ? 'w-96' : 'w-0'} transition-width duration-1000 whitespace-nowrap overflow-hidden`}>&nbsp; TODO: Music Banner Here</div>
+      <ActivityIcon width={15} height={15}/>
+      <div className={`${hovered ? 'w-80' : 'w-0'} transition-width duration-1000 whitespace-nowrap overflow-hidden flex justify-between`}>
+        <div className="pl-2">Jakob Ahlbom - When It Rains It Pours</div>
+        <div className="flex flex-row gap-2">
+          <SkipBackIcon size={15}/>
+          <PlayIcon size={15}/>
+          <SkipForwardIcon size={15}/>
+        </div>
+      </div>
     </div>
   )
 }
