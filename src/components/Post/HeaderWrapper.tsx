5650bc1ddb887d321cfc634eb1c2a4165ac0a0a1
flipersclub <lffonseca1@me.com>
2025-03-30 10:27:19 +0100

    Fixed placeholder for post title

--- src/components/Post/HeaderWrapper.tsx
+++ src/components/Post/HeaderWrapper.tsx
@@ -10,8 +10,8 @@ const HeaderWrapper: React.FunctionComponent<TProps & PropsWithChildren> = ({
   return (
     <>
       <header>
-        <div className={`h-96 flex items-center justify-center`}>
-          <div className="text-center py-6 mt-6">
+        <div className={`w-full h-96 flex items-center justify-center`}>
+          <div className="w-full text-center py-6 mt-6">
             {page && <h2 className="uppercase font-sans-serif text-stone-400 tracking-widest">{page}</h2>}
             <div className={'text-stone-100'}>
               {children}
