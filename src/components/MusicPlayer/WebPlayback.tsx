43499b1ccf5007fcf9cbe1535b15b3c79d72f96e
flipersclub <lffonseca1@me.com>
2025-10-04 18:40:16 +0100

    Improve Spotify player behavior and UI for paused/inactive states
    Adjusted width handling based on player state and refined UI display logic for paused tracks or inactive devices. Simplified conditional rendering to enhance readability and maintainability.

--- src/components/MusicPlayer/WebPlayback.tsx
+++ src/components/MusicPlayer/WebPlayback.tsx
@@ -24,33 +24,32 @@ const WebPlayback = ({ open }: TWebPlaybackProps) => {
   } = useSpotifyPlayer()
 
   return (
-    <div className={`${open || isActive ? 'w-128' : 'w-0'} transition-width duration-1000 whitespace-nowrap overflow-hidden flex justify-between`}>
+    <div className={`${open || !isPaused ? 'w-128' : 'w-0'} transition-width duration-1000 whitespace-nowrap overflow-hidden flex justify-between`}>
       <div className="pl-2 max-w-96 overflow-hidden mt-1">{currentTrack
         ? `${currentTrack.artists?.[0]?.name} - ${currentTrack.name}`
-        : ''}</div>
-      {(isActive) ? (
-        <div className="flex flex-row gap-2">
-          <button className={'h-5 w-5'} onClick={previous}>
-            <SkipBackIcon className={'h-5 w-5'}/>
-          </button>
-          {(isPaused) ? (
-            <button className={'h-5 w-5'} onClick={() => play()}>
-              <PlayIcon className={'h-5 w-5'}/>
-            </button>
-          ) : (
-            <button className={'h-5 w-5'} onClick={pause}>
-              <PauseIcon className={'h-5 w-5'}/>
-            </button>
-          )}
-          <button className={'h-5 w-5'} onClick={next}>
-            <SkipForwardIcon className={'h-5 w-5'}/>
-          </button>
-        </div>
-      ) : (
-        <button className={'h-5 w-5'} onClick={activate}>
-          <SpeakerIcon className={'h-5 w-5'}/>
+        : !isPaused ? 'Playing on another device' : ''}</div>
+      <div className="flex flex-row gap-2">
+        <button className={'h-5 w-5'} onClick={previous}>
+          <SkipBackIcon className={'h-5 w-5'}/>
         </button>
-      )}
+        {(isPaused) ? (
+          <button className={'h-5 w-5'} onClick={() => play()}>
+            <PlayIcon className={'h-5 w-5'}/>
+          </button>
+        ) : (
+          <button className={'h-5 w-5'} onClick={pause}>
+            <PauseIcon className={'h-5 w-5'}/>
+          </button>
+        )}
+        <button className={'h-5 w-5'} onClick={next}>
+          <SkipForwardIcon className={'h-5 w-5'}/>
+        </button>
+        {(!isActive) && (
+          <button className={'h-5 w-5'} onClick={activate}>
+            <SpeakerIcon className={'h-5 w-5'}/>
+          </button>
+        )}
+      </div>
     </div>
   )
 }
